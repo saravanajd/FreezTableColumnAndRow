20d3a5c089596dac9c9cfd9d2e29b4788a4bcfaa
saravana jd <saravana.jd@gmail.com>
2019-04-29 09:57:37 +0530

    Freez Table using sticky

---
+++ app.js
@@ -0,0 +1,30 @@
+const freezRow = (element, lastColumnIndex) => {
+    document.querySelectorAll(`${element} tr`).forEach(function (tr) {
+
+        let td = Array.from(tr.querySelectorAll('td'))
+            .slice(0, lastColumnIndex);
+
+        if (td.length < lastColumnIndex) {
+            td = Array.from(tr.querySelectorAll('th'))
+                .slice(0, lastColumnIndex);
+
+            if (td.length < lastColumnIndex)
+                return;
+        }
+
+        td.forEach((x, i) => {
+            x.style.position = 'sticky';
+            x.style.position = '-webkit-sticky';
+            x.style.background = '#fafafa';
+            x.style.minWidth = '100px';
+            x.style.maxWidth = '100px';
+            x.style.width = '100px';
+            x.style.overflow = 'hidden';
+            x.style.textOverflow = 'ellipsis';
+           // x.style.border = '0';
+            x.style.left = `${i * 100}px`;
+        });
+    });
+}
+
+freezRow('table',3);
